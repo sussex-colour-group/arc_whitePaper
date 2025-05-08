67c403ebf1cdce692e8b44eadd89e4cd8b764a78
da5nsy <dannygarside@outlook.com>
2025-05-08 18:12:58 +0100

    update getLocalPaths.m

--- getLocalPaths.m
+++ getLocalPaths.m
@@ -5,17 +5,13 @@ function localPaths = getLocalPaths
 % It's ignored using this technique: https://stackoverflow.com/a/39776107/6464224
 % To unignore it run `git update-index --no-skip-worktree getLocalPaths.m`
 
-error("You haven't specified your local paths yet.")
+localPaths.GoProRawData = '';
+localPaths.GoProProcessedData = '';
 
-localPaths.GoProRawData = "";
-localPaths.GoProProcessedData = "";
+localPaths.NLRawData = '';
+localPaths.NLProcessedData = '';
 
-localPaths.NLRawData = "";
-localPaths.NLProcessedData = "";
-
-localPaths.HSRawData = "";
-localPaths.HSProcessedData = "";
-
-localPaths.saveLocation = "";
+localPaths.HSRawData = '';
+localPaths.HSProcessedData = '';
 
 end
