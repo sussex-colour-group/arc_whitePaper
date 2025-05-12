f47c680cc18f90f6974585b59de3cf70e20c5fc1
Danny Garside <dannygarside@outlook.com>
2025-05-12 22:35:07 +0100

    clarify comment

--- getLocalPaths.m
+++ getLocalPaths.m
@@ -3,6 +3,7 @@ function localPaths = getLocalPaths
 % This file isn't tracked by git, because it will vary per user/OS.
 % Fill it in, and remove the error line.
 % It's ignored using this technique: https://stackoverflow.com/a/39776107/6464224
+% To set it to be ignored run `git update-index --skip-worktree getLocalPaths.m`
 % To unignore it run `git update-index --no-skip-worktree getLocalPaths.m`
 
 localPaths.GoProRawData = '';
