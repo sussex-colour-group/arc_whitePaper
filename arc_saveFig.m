6cb792c302cc30d85900b9b1306641590170f286
Danny Garside <dannygarside@outlook.com>
2025-06-30 12:57:03 +0100

    switch on pdf figure output

--- arc_saveFig.m
+++ arc_saveFig.m
@@ -2,10 +2,14 @@ function arc_saveFig(filename,meta)
 
 set(findall(gcf,'-property','FontName'),'FontName','San Serif');
 
-% print(gcf,'-vector','-dsvg',...
-%     [saveLocation,filename,'_',meta.figType,'.svg'])
-
 print(gcf,'-dpng',...
     [filename,'.png'])
+
+% print(gcf,'-vector','-dpdf',...
+%     [filename,'.pdf'])
+
+exportgraphics(gcf,[filename,'.pdf'],... 
+    'ContentType','vector',...
+    'BackgroundColor','none')
 
 end
